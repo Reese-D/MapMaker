c3209ec3fa30a9f9d0688fe1365d958dbbf6581b
Reese De Wind <reese.dewind@gmail.com>
2020-05-09 14:26:27 -0400

    updating gitignore

--- Hello2.hs
+++ Hello2.hs
@@ -1,15 +1,36 @@
 import Graphics.UI.GLUT
 import Graphics.Rendering.OpenGL
-import Graphics.GLUtil
+--import Graphics.GLUtil
 
 
 {- run these to build autocompletion tags
    visit-tags-table
    company-etags -}
+
+
+stuff3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+stuff3 f (a, b, c) = f a b c
+
+myPoints :: [Vertex3 GLfloat]
+myPoints = map (stuff3 Vertex3) [(sin(2*pi*k/12), cos(2*pi*k/12), 0) | k <- [1..12]]
+
 main :: IO ()
 main = do
-  gl
+  (_programName, _arguments) <- getArgsAndInitialize
+  _window <- createWindow "hello world"
+  displayCallback $= display
+  reshapeCallback $= Just reshape
+  glutInitDisplayMode 1
   mainLoop
 
+reshape :: ReshapeCallback
+reshape size = do
+  viewport $= (Position 0 0, size)
+  postRedisplay Nothing
+  
+display :: DisplayCallback
+display = do
+  clear [ ColorBuffer ]
+  flush
 
   
